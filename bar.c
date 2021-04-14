2d53a55b493854bc16179e00295a1c5c27967f59
h-arieff <80794782+h-arieff@users.noreply.github.com>
2021-04-14 19:27:37 -0400

    started working on bar.{c,h}

---
+++ bar.c
@@ -0,0 +1,5 @@
+#include "bar.h"
+char*b_disp="|-|-|-|-|-";
+void b_xylh(int*tf);//tf must be 4*sizeof(int)
+void b_setup();
+void b_cleanup();
---
+++ bar.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "includes.h"
+char*b_disp;
+void b_xylh(int*tf);//tf must be 4*sizeof(int)
+void b_setup();
+void b_cleanup();
